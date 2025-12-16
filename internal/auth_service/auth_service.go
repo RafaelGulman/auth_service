8b0c60995f490ae6c02643c5eb6db2f064e48e40
Rafael Gilmanov <Gulman04@outlook.com>
2025-12-16 12:54:22 +0500

    Added function which generate random password
    Added comments

--- internal/auth_service/auth_service.go
+++ internal/auth_service/auth_service.go
@@ -5,10 +5,12 @@ import (
 	"errors"
 	mathV2 "math/rand/v2"
 	"net/http"
+	"strings"
 )
 
 var accounts []Account
 
+// Function is generate random ASCII symbol from range which defined in RangeType
 func generateFromRange(rt ...RangeType) string {
 	tempRange := rt[mathV2.IntN(len(rt))]
 	if tempRange.Max-tempRange.Min == 0 {
@@ -17,6 +19,36 @@ func generateFromRange(rt ...RangeType) string {
 	return string(mathV2.IntN(tempRange.Max-tempRange.Min) + tempRange.Min)
 }
 
+// Generate random password
+func GenerateRandomPasswordV2() string {
+	var completePassword string
+
+	completePassword += generateFromRange(SpecSymbRange...)
+	completePassword += generateFromRange(DigitCaseRange...)
+	completePassword += generateFromRange(UpperCaseRange...)
+	completePassword += generateFromRange(DownerCaseRange...)
+
+	itterationCount := mathV2.IntN(8) + 4
+
+	for i := 0; i < itterationCount; i++ {
+		switch mathV2.IntN(4) {
+		case 0:
+			completePassword += generateFromRange(SpecSymbRange...)
+		case 1:
+			completePassword += generateFromRange(DigitCaseRange...)
+		case 2:
+			completePassword += generateFromRange(UpperCaseRange...)
+		case 3:
+			completePassword += generateFromRange(DownerCaseRange...)
+		}
+	}
+	newStrArr := strings.Fields(completePassword)
+	mathV2.Shuffle(len(newStrArr), func(i, j int) { newStrArr[i], newStrArr[j] = newStrArr[j], newStrArr[i] })
+	completePassword = strings.Join(newStrArr, "")
+	return completePassword
+}
+
+// Check string on compliance propertis of passwords ()
 func PasswordValidation(pass string) []error {
 	var allErrors []error
 

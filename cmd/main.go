b6364d059728ad6a66819fa1c0f2ca81bd9d901e
Rafael Gilmanov <Gulman04@outlook.com>
2025-12-21 13:37:38 +0500

    added comment

--- cmd/main.go
+++ cmd/main.go
@@ -8,12 +8,12 @@ import (
 )
 
 func main() {
+	//define functions which will be use
 	methodsStructs := make(map[string]server.HttpMethod)
-
 	methodsStructs["CreateAccount"] = *server.NewHttpMethod(server.POST, auth_service.CreateAccount, "/accounts")
 	methodsStructs["GetAccounts"] = *server.NewHttpMethod(server.GET, auth_service.GetAccounts, "/accounts")
 
+	//Init and start service
 	mainServer := server.NewController(*mux.NewRouter(), methodsStructs)
-
-	mainServer.Start(":8080")
+	mainServer.Start(":8081")
 }
